5ea626bdfcf535a37e420464aa671790ebfc28b6
HaroldObasi <haroldobasi2k16@gmail.com>
2023-01-15 18:49:22 +0100

    import name changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import "./App.css";
-import Header from "./Header.js";
-import Home from "./Screens/Home";
+import SortingVisualizer from "./Screens/SortingVisualizer/SortingVisualizer";
+import Graph from "./Screens/Graph";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<SortingVisualizer />} />
+        <Route path="/graph" element={<Graph />} />
       </Routes>
     </Router>
   );
